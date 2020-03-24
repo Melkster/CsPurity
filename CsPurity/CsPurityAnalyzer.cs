ddecaff5d428eb0ee8c9cab35665740fea73d371
Melker Österberg <melkerosterberg@gmail.com>
2020-03-24 12:02:15 +0100

    Add a simple first implementation of a purity analyzer
    The analyzer looks at each method's identifiers and whether or any of
    them were defined outside their method.

--- CsPurity/CsPurityAnalyzer.cs
+++ CsPurity/CsPurityAnalyzer.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using static System.Console;
 
 namespace CsPurity
 {
@@ -6,13 +12,64 @@ namespace CsPurity
     {
         public static bool Analyze(string text)
         {
+            var tree = CSharpSyntaxTree.ParseText(text);
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var compilation = CSharpCompilation.Create("HelloWorld")
+                .AddReferences(
+                    MetadataReference.CreateFromFile(
+                        typeof(string).Assembly.Location
+                    )
+                ).AddSyntaxTrees(tree);
+            var model = compilation.GetSemanticModel(tree);
+
+            var methodDeclarations = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+            foreach (var methodDeclaration in methodDeclarations)
+            {
+                var identifierNames = methodDeclaration.DescendantNodes().OfType<IdentifierNameSyntax>();
+                foreach (var identifierName in identifierNames)
+                {
+                    var identifierSymbol = (VariableDeclaratorSyntax)model
+                        .GetSymbolInfo(identifierName)
+                        .Symbol
+                        .DeclaringSyntaxReferences.Single() // TODO: look at all references
+                        .GetSyntax();
+                    var methodAncestors = identifierSymbol.Ancestors().OfType<MethodDeclarationSyntax>();
+                    bool methodIsPure = false;
+
+                    if (methodAncestors.Any()) methodIsPure = methodAncestors.First() == methodDeclaration;
+                    return methodIsPure;
+                }
+            }
             return true;
         }
 
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var file = (@"
+                namespace TestApp
+                {
+                    class C1
+                    {
+                        string bar = ""subtract"";
+                        string subtract()
+                        {
+                            return bar;
+                        }
+
+                        class C2
+                        {
+                            string add()
+                            {
+                                string foo = ""add"";
+                                return foo;
+                            }
+                        }
+                    }
+                }
+            ");
+
+            Analyze(file);
         }
     }
 }
--- CsPurityTests/UnitTest.cs
+++ CsPurityTests/UnitTest.cs
@@ -7,15 +7,31 @@ namespace CsPurityTests
     public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestBasicImpurity()
         {
             var file = (@"
                 class C1
                 {
                     int bar = 42;
-                    void add()
+                    void foo()
                     {
-                        bar = 43;
+                        return bar;
+                    }
+                }
+            ");
+            Assert.IsFalse(CsPurityAnalyzer.Analyze(file));
+        }
+
+        [TestMethod]
+        public void TestBasicPurity()
+        {
+            var file = (@"
+                class C1
+                {
+                    void foo()
+                    {
+                        int bar = 42;
+                        return bar;
                     }
                 }
             ");
